aca8516003e57406c5c21abb90b4ac87923860d5
matmarchew <matmarchew@gmail.com>
2018-12-11 16:01:59 +0100

    Create first unit test

---
+++ Server/src/test/java/UnitTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class UnitTest {
+    @Test
+    void shouldPassed() {
+        Assertions.assertTrue(true);
+    }
+}
